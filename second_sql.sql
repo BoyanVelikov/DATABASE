c5555c259d842a89f417bf8111d5ff12cde09cb7
Boyan Velikov <boyanvelikov@gmail.com>
2022-11-01 08:39:54 +0200

    Add files via upload

---
+++ second_sql.sql
@@ -0,0 +1,16 @@
+-- SELECT 
+-- first_name, 
+-- last_name, 
+-- points, 
+-- (points + 10) * 100 AS 'discount factor'
+-- from customers;
+
+-- SELECT DISTINCT state
+-- FROM customers
+
+SELECT name, unit_price, unit_price * 1.1 AS new_price
+FROM products
+
+
+
+
