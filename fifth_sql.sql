719e2060e4a730b464227841bda5a406c5fbcf5b
Boyan Velikov <boyanvelikov@gmail.com>
2022-11-04 08:39:59 +0200

    Add files via upload

---
+++ fifth_sql.sql
@@ -0,0 +1,15 @@
+-- SELECT * 
+-- FROM customers
+-- WHERE first_name REGEXP 'elka|ambur'
+
+-- SELECT * 
+-- FROM customers
+-- WHERE last_name REGEXP 'ey$|on$'
+
+-- SELECT * 
+-- FROM customers
+-- WHERE last_name REGEXP '^my|se'
+
+SELECT * 
+FROM customers
+WHERE last_name REGEXP 'br|bu'
