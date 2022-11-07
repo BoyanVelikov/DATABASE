6c7d20111c06c75ab93d63002a92b5c6c4de8447
Boyan Velikov <boyanvelikov@gmail.com>
2022-11-07 08:31:56 +0200

    Update seventh_sql.sql

--- seventh_sql.sql
+++ seventh_sql.sql
@@ -2,6 +2,23 @@
 -- FROM customers
 -- ORDER BY first_name DESC
 
-SELECT *
-FROM customers
-ORDER BY state, first_name
+-- SELECT *
+-- FROM customers
+-- ORDER BY state, first_name
+
+-- SELECT *
+-- FROM customers
+-- ORDER BY state DESC, first_name DESC
+
+-- SELECT first_name, last_name
+-- FROM customers
+-- ORDER BY birth_date
+
+-- SELECT first_name, last_name, 10 as points
+-- FROM customers
+-- ORDER BY points, first_name 
+
+SELECT *, quantity * unit_price AS total_price
+FROM order_items
+WHERE order_id = 2
+ORDER BY total_price DESC  
