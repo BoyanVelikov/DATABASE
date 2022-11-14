b89ff2b256536892b49073f561e6f63abc72e6ba
Boyan Velikov <boyanvelikov@gmail.com>
2022-11-14 09:32:09 +0200

    Update 13th.sql

---
+++ 13th.sql
@@ -0,0 +1,28 @@
+USE sql_store;
+
+-- SELECT 
+-- 	c.customer_id,
+--     c.first_name,
+--     o.order_id,
+--     sh.name AS shipper
+-- FROM customers c
+-- LEFT JOIN orders o
+-- 	ON c.customer_id = o.customer_id
+-- LEFT JOIN shippers sh
+-- 	ON o.shipper_id = sh.shipper_id
+-- ORDER BY c.customer_id
+
+SELECT 
+	o.order_date,
+    o.order_id,
+    c.first_name,
+    s.name,
+    os.name
+FROM customers c
+JOIN orders o
+	ON c.customer_id = o.customer_id
+LEFT JOIN shippers s
+	ON o.shipper_id = s.shipper_id
+JOIN order_statuses os
+
+
