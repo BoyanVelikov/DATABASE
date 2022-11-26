1a8c4be76d13463a83dc6afaa28ae3791f0e9176
Boyan Velikov <boyanvelikov@gmail.com>
2022-11-26 09:47:44 +0200

    Add files via upload

---
+++ 22th.sql
@@ -0,0 +1,22 @@
+-- UPDATE invoices
+-- 	SET payment_total = invoice_total * 0.5,
+--     payment_date = due_date
+
+-- SELECT * 
+-- FROM invoices
+-- WHERE payment_date IS NULL
+
+-- USE sql_store;
+
+-- UPDATE orders
+-- 	SET comments = 'Gold customer'
+-- WHERE customer_id IN
+
+-- (SELECT customer_id
+-- FROM customers
+-- WHERE points > 3000)
+
+
+
+
+    
